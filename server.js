0c6e85db37ff33e67178e6d8d7ae83546fb6cb3d
jay speights <jaymspeights@gmail.com>
2018-01-19 13:45:08 -0600

    added https

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 var express = require ('express');
 var app = express();
 
+var http = require('http');
+var https = require('https');
+var privateKey  = fs.readFileSync('/etc/letsencrypt/live/edh.life/private.key', 'utf8');
+var certificate = fs.readFileSync('/etc/letsencrypt/live/edh.life/cert.crt', 'utf8');
+
+var credentials = {key: privateKey, cert: certificate};
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
@@ -11,4 +18,8 @@ app.get('/.well-known/*', (req, res) => {
   res.sendFile(__dirname + req.originalUrl);
 });
 
-app.listen(80);
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(80);
+httpsServer.listen(443);
